a476502eb977bcab5ed9cd88af81830886c3bcbd
Avighna <avighnakc@gmail.com>
2021-10-03 19:39:34 +0530

    Made update.py work.

--- update.py
+++ update.py
@@ -1,23 +1,23 @@
 import os
 import shutil
+import requests
 import zipfile
 
+url = "https://raw.githubusercontent.com/avighnac/math_new/main/code.zip"
+
 # Download the file from `url` and save it locally under `file_name`:
-url = "https://codeload.github.com/avighnac/math_new/zip/refs/heads/main"
+r = requests.get(url)
 
-if not os.path.exists("math_new.zip"):
-    import urllib.request
-    urllib.request.urlretrieve(url, "math_new.zip")
+with open("code.zip", "wb") as code:
+    code.write(r.content)
 
-file_name = zipfile.ZipFile("math_new.zip")
-
+# Delete the directory if it exists
 if os.path.exists("code"):
     shutil.rmtree("code")
 
-# Extract code from the archive
-for file in file_name.namelist():
-    if file.startswith('code/'):
-        file_name.extract(file)
+# Extract the zip file
+with zipfile.ZipFile("code.zip", 'r') as zip_ref:
+    zip_ref.extractall(".")
 
-# Remove the zip file
-os.remove("math_new.zip")
+# Delete the zip file
+os.system("rm code.zip")
