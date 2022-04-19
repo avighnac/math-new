3f98d937a1fa73a49ce5e98165a453a026d7cbc4
Avighna <avighnakc@gmail.com>
2022-04-19 15:20:17 +0530

    Fixed isPrime.h tests.

--- math++/code/source/algorithms/old/PrimeFinder/isPrime.h
+++ math++/code/source/algorithms/old/PrimeFinder/isPrime.h
@@ -21,30 +21,31 @@ bool isPrime (long long x)
     return counter == 2;
 }
 
-bool isPrime(std::string x) {
+bool isPrime(std::string x, bool testing = false) {
   if (!((x[x.length() - 1] - 48) & 1))
     return false;
 
-  if (algebric_num::smaller_than(
-          algebric_num::algebric_number(x),
-          algebric_num::algebric_number("9223372036854775807")))
-    return isPrime(std::stoll(x));
+  if (!testing)
+    if (algebric_num::smaller_than(
+            algebric_num::algebric_number(x),
+            algebric_num::algebric_number("9223372036854775807")))
+      return isPrime(std::stoll(x));
 
   std::string i = "1";
-  std::string counter = "0";
+  int counter = 0;
 
   while (algebric_num::smaller_than(algebric_num::algebric_number(i),
                                     algebric_num::algebric_number(x))
                  || i == x) {
     if (modulus(x, i) == "0") {
-      counter = add(counter, "1");
-      if (algebric_num::greater_than(algebric_num::algebric_number(counter), algebric_num::algebric_number("2")))
+      counter++;
+      if (counter > 2)
         return false;
     }
     i = add(i, "1");
   }
 
-  return counter == "2";
+  return counter == 2;
 }
 
 #endif
