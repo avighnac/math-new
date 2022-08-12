1ba01fbedfa365ccf67b40824dc2f80fc3760267
avighnac <avighnakc@gmail.com>
2022-08-12 22:20:05 +0530

    Fixed testing software.

--- math++/testing_software/test.sh
+++ math++/testing_software/test.sh
@@ -1,2 +1,2 @@
-g++ testing_software.cpp ../../basic_math_operations/basic_math_operations.cpp -O3 -o testing_software
+g++ testing_software.cpp ../../basic_math_operations/basic_math_operations.cpp -O3 -o "testing_software"
 ./testing_software
