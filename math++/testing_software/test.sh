c24dda41c26745e204e4dd9788ee906d58432027
avighnac <avighnakc@gmail.com>
2022-09-03 19:33:23 +0530

    Added .gitattributes.

--- math++/testing_software/test.sh
+++ math++/testing_software/test.sh
@@ -1,2 +1,4 @@
-g++ testing_software.cpp ../../basic_math_operations/basic_math_operations.cpp -O3 -o "testing_software"
+nasm -felf64 "../../basic_math_operations/Addition Algorithm/add.asm" -o add.o
+g++ testing_software.cpp ../../basic_math_operations/basic_math_operations.cpp add.o -O3 -o "testing_software"
 ./testing_software
+rm add.o
